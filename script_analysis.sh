39fb7be8b0cecce5c5cdb891725fa76d4f210a5a
yuuka-kanakubo <y-kanakubo-75t@eagle.sophia.ac.jp>
2021-03-13 00:28:02 +0900

    CMakeLists.txt: Changed executable's name.

--- script_analysis.sh
+++ script_analysis.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-#DIR="../DCCI/data/20210212_PBPB_PT0REF0.9_SIGMA0.5_tau0Unlimited_for_DCCI_CF_T165_NOparticlization"
-DIR="../DCCI/data/20210302_PP_PT0REF1.8_SIGMA0.5_tau0Unlimited_for_DCCI_CF_T165_MB_0Kto100K_NOparticlization"
+DIR="../DCCI/data/20210212_PBPB_PT0REF0.9_SIGMA0.5_tau0Unlimited_for_DCCI_CF_T165_NOparticlization"
+#DIR="../DCCI/data/20210302_PP_PT0REF1.8_SIGMA0.5_tau0Unlimited_for_DCCI_CF_T165_MB_0Kto100K_NOparticlization"
 #DIR="../pythia8244/default_pythia_mymain/data/20210305_DEFAULT_PP13TEV"
 EV="ev"
 EXT="hadronFinal_corecorona_weakStop_wcol.txt"
 #EXT="default.txt"
-outputdir="MEANPTMULTI_DCCI_PP"
-n=100000
+outputdir="MEANPTMULTI_DCCI_PBPB"
+n=5000
 
 
 #Do not modify this.
@@ -43,6 +43,6 @@ fi
 
 ./analysis \
  -n $n -outdir ${outputdir} -dir ${DIR} -f ${EV} -ext ${EXT} \
- --INEL_lg_0 \
+ --HI \
  > ${log_dname}${log_fname}.log  2>&1  &
 
