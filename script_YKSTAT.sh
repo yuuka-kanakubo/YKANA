298997a3cf0a51eefa3d033859c8d07138281fbc
yuuka-kanakubo <y-kanakubo-75t@eagle.sophia.ac.jp>
2021-03-13 10:45:34 +0900

    script: Fixed executable's name and changed file name.

--- script_YKSTAT.sh
+++ script_YKSTAT.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-DIR="../DCCI/data/20210212_PBPB_PT0REF0.9_SIGMA0.5_tau0Unlimited_for_DCCI_CF_T165_NOparticlization"
-#DIR="../DCCI/data/20210302_PP_PT0REF1.8_SIGMA0.5_tau0Unlimited_for_DCCI_CF_T165_MB_0Kto100K_NOparticlization"
+#DIR="../DCCI/data/20210212_PBPB_PT0REF0.9_SIGMA0.5_tau0Unlimited_for_DCCI_CF_T165_NOparticlization"
+DIR="../DCCI/data/20210302_PP_PT0REF1.8_SIGMA0.5_tau0Unlimited_for_DCCI_CF_T165_MB_0Kto100K_NOparticlization"
 #DIR="../pythia8244/default_pythia_mymain/data/20210305_DEFAULT_PP13TEV"
 EV="ev"
-EXT="hadronFinal_corecorona_weakStop_wcol.txt"
+EXT="hadronFinal_corecoronaona_weakStop.txt"
 #EXT="default.txt"
-outputdir="MEANPTMULTI_DCCI_PBPB"
-n=5000
+outputdir="MEANPTMULTI_DCCI_WOCOL_PP"
+n=100000
 
 
 #Do not modify this.
@@ -41,8 +41,8 @@ fi
 # --twosub  \
 # --vn \
 
-./analysis \
+./YKSTAT \
  -n $n -outdir ${outputdir} -dir ${DIR} -f ${EV} -ext ${EXT} \
- --HI \
+ --INEL_lg_0 \
  > ${log_dname}${log_fname}.log  2>&1  &
 
