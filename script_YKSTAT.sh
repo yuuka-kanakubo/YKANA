fc20eeb6fd0002f82a6d7b612964d0b1d7fa5bb1
yuuka-kanakubo <y-kanakubo-75t@eagle.sophia.ac.jp>
2021-05-10 17:23:46 +0900

    Added option -DJET_PRAC

--- script_YKSTAT.sh
+++ script_YKSTAT.sh
@@ -2,11 +2,11 @@
 
 #DIR="../DCCI/data/20210324_PBPB_PT0REF0.9_SIGMA0.5_tau0Unlimited_for_DCCI_CF_T165_MB_0Kto10K_NOparticlization"
 #DIR="../DCCI/data/20210322_PP_PT0REF1.8_SIGMA0.5_MB_tau0Unlimited_for_DCCI_CF_T165_0Kto300K_NOparticlization"
-DIR="../pythia8244/default_pythia_mymain/data/20210325_DEFAULT_PBPB2760GEV_weakStop_100K"
+DIR="../pythia8244/sample_pythia/data/20210510_JetsTEST"
 EV="ev"
-EXT="default.txt"
-outputdir="V22_PBPB_PYTHIA_LESSSTAT"
-n=10000
+EXT="jetinfo.txt"
+outputdir="AJ"
+n=1000
 
 
 #Do not modify this.
@@ -41,7 +41,12 @@ fi
 # --threesub  \
 # --4particle \
 
+for i in `seq 0.2 0.2 0.2`
+do
+for j in `seq 0.5 1.0 0.5`
+do
 ./YKSTAT \
- -n $n -outdir ${outputdir} -dir ${DIR} -f ${EV} -ext ${EXT} \
- --HI \
- > ${log_dname}${log_fname}.log  2>&1  &
+ -n $n -outdir ${outputdir}_a$i"_s"$j -dir ${DIR}_a$i"_s"$j -f ${EV} -ext ${EXT}
+# > ${log_dname}${log_fname}_a$i"_s"$j.log  2>&1  &
+done
+done
